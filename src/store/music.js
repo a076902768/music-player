fd9ca8347aba15e785d4ff6493da891ef352101d
Wei Bo Zheng <47736792+a076902768@users.noreply.github.com>
2022-04-26 18:56:07 +0800

    feat: 建立music.js(音樂相關變數及邏輯寫在這，vue環境內共用)

---
+++ src/store/music.js
@@ -0,0 +1,31 @@
+// 音樂控制相關邏輯及相關音樂變數寫在此
+import { reactive } from 'vue'
+
+const musicInfo = reactive({
+
+})
+
+const playSong = () => {}
+
+const pauseSong = () => {}
+
+const nextSong = () => {}
+
+const previousSong = () => {}
+
+const controlAudioVol = () => {}
+
+const controlAudioTime = () => {}
+
+const uploadSong = () => {}
+
+export default {
+  musicInfo,
+  playSong,
+  pauseSong,
+  nextSong,
+  previousSong,
+  controlAudioVol,
+  controlAudioTime,
+  uploadSong
+}
